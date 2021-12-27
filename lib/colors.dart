b4dc9241de17667a6c62c1d356d4413ae8748e07
zayniddin <tuit0778@gmail.com>
2021-12-27 22:58:57 +0500

    first commit

---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColors {
+
+  static Color white = const Color(0xFFE5E5E5);
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/screens/auth_page.dart';
+import 'package:news_app/screens/home_page.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "News App",
+      home: AuthPage(),
+    );
+  }
+}
