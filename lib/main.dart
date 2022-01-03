c677519f9b8aa4f78b56696f04076af437cd1014
muxtorjon <muxtorovinomjon0227@gmail.com>
2022-01-03 23:58:38 +0500

    shu yerga kirib hammasini tanlab commit and push bosi

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,13 @@ class NewsApp extends StatelessWidget {
   final String change2 = "shunga oxshatob proejctni kotarasiz";
   final String change3 = "yani ozgairshlar qilasiz";
   final String change4 = "endi ozgairshlarni saqlaymiz buning uchun // git add . // yozamiz";
+  final String change5 = "agar sezgan bolsangiz qoshganimizdan keyin rang ozgardi";
+  final String change6 = "git add tochka qilsez hamma fayllar qoshiladi agar bitta gayl qoshaman desez git add main.dart qilib noimini yozishiz kera";
+  final String change7 = "hozir men yozgan korinishda commit yozasiz yan nima ishlar qilingani";
+  final String change8 = "identifikatsiya";
+  final String change9 = "hozi biza giot commit qilib ozgairshlarni umuman olganda kodlarni local save qildik endi uni globalga push qilamiz";
+  final String change10= "bro zor usuli bor edi esdan chiqib binnima qilib sizni boshizi qotiribman hoz.....";
+  final String sss= "aytaylik change boldi endi";
 
   @override
   Widget build(BuildContext context) {
