1a4c6b51d1bcae81948d9b749f93f33a8b1e559b
zayniddin <tuit0778@gmail.com>
2022-01-07 21:49:59 +0500

    nimadir ish qildim

---
+++ lib/models/my_topic.dart
@@ -0,0 +1,30 @@
+class MyTopic {
+  String title;
+  bool isSelected;
+
+  @override
+  String toString() {
+    return 'MyTopic{title: $title, isSelected: $isSelected}';
+  }
+
+  MyTopic(this.title, {this.isSelected = false});
+
+  //TODO : MOVE LIST BELOW to another file
+  static List<MyTopic> topics = [
+    MyTopic('🎶 Music'),
+    MyTopic('💼 Business'),
+    MyTopic('🎨 Design'),
+    MyTopic('📖 Book'),
+    MyTopic('📷 Photography'),
+    MyTopic('🏛 Architecture'),
+    MyTopic('✍🏻 Writing'),
+    MyTopic('‍🏫 Education'),
+    MyTopic('🎮 Entertainment'),
+    MyTopic('🍃 Nature'),
+    MyTopic('🏝 Travel'),
+    MyTopic('🏓 Sports'),
+    MyTopic('⚕️ Health'),
+    MyTopic('🍳 Recipes'),
+    MyTopic('💪 Gym & Fitness'),
+  ];
+}
