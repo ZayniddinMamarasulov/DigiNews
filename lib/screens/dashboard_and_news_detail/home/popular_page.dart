c8ec4cd511e6fee5c60d7a90ed483f9a7f96934d
Abbos Mamadaliyev <abbbos166@gmail.com>
2022-01-11 21:05:36 +0500

    latest news added in popular page

--- lib/screens/dashboard_and_news_detail/home/popular_page.dart
+++ lib/screens/dashboard_and_news_detail/home/popular_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:news_app/screens/dashboard_and_news_detail/home/components/popular_carousel.dart';
+import 'package:news_app/utils/static_data.dart';
 
 class PopularPage extends StatefulWidget {
   const PopularPage({Key? key}) : super(key: key);
@@ -20,37 +22,89 @@ class _PopularPageState extends State<PopularPage> {
     return Column(
       children: [
         PopularCarousel(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                'Latest News',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Text(
+                'See more',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.redAccent,
+                ),
+              ),
+            ],
+          ),
+        ),
         Expanded(
           child: Container(
             margin: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: ListView(
-              children: [
-                Container(
-                  height: 60,
-                  color: Colors.red,
-                  margin: EdgeInsets.all(4.0),
-                ),
-                Container(
-                  height: 60,
-                  color: Colors.red,
-                  margin: EdgeInsets.all(4.0),
-                ),
-                Container(
-                  height: 60,
-                  color: Colors.red,
-                  margin: EdgeInsets.all(4.0),
-                ),
-                Container(
-                  height: 60,
-                  color: Colors.red,
-                  margin: EdgeInsets.all(4.0),
-                ),
-                Container(
-                  height: 60,
-                  color: Colors.red,
-                  margin: EdgeInsets.all(4.0),
-                ),
-              ],
+            child: ListView.builder(
+              itemCount: StaticData.latestNews.length,
+              itemBuilder: (BuildContext context, int index) {
+                var list = StaticData.latestNews[index];
+                return Container(
+                  height: 96,
+                  padding: const EdgeInsets.only(top: 12),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 96,
+                        width: 80,
+                        margin: const EdgeInsets.only(right: 12.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: NetworkImage(list.image),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 250,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(list.category),
+                            Text(
+                              list.title,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    const Text('1 days ago | '),
+                                    Text('${list.readingTime} mins read'),
+                                  ],
+                                ),
+                                const Icon(
+                                  Icons.bookmark_add_outlined,
+                                  color: Colors.grey,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
             ),
           ),
         )
--- lib/utils/static_data.dart
+++ lib/utils/static_data.dart
@@ -1,5 +1,6 @@
 import 'package:news_app/models/author.dart';
 import 'package:news_app/models/carousel_item.dart';
+import 'package:news_app/models/news.dart';
 
 class StaticData {
   static List<CarouselItem> popularBanners = [
@@ -18,5 +19,40 @@ class StaticData {
         'https://storage.kun.uz/source/8/5X73logbix8KzHlW2yu-16dIDHIRgCGd.jpg',
         Author('kun.uz', 'kun.uz rasm'),
         7),
+  ];
+
+  static List<News> latestNews = [
+    News(
+      category: 'Mahalliy',
+      image:
+          'https://storage.kun.uz/source/8/ZXanKouRm369UYPG1m2hfHVziJbnbUJZ.jpg',
+      title: 'Maktablarda ham qishki ta’til uzaytirildi',
+      readingTime: 4,
+      isSaved: false,
+    ),
+    News(
+      category: 'Texno',
+      image:
+          'https://storage.kun.uz/source/8/5X73logbix8KzHlW2yu-16dIDHIRgCGd.jpg',
+      title: 'Honor buklanuvchi smartfonini taqdim etdi',
+      readingTime: 7,
+      isSaved: false,
+    ),
+    News(
+      category: 'Texno',
+      image:
+          'https://storage.kun.uz/source/8/5X73logbix8KzHlW2yu-16dIDHIRgCGd.jpg',
+      title: 'Honor buklanuvchi smartfonini taqdim etdi',
+      readingTime: 7,
+      isSaved: false,
+    ),
+    News(
+      category: 'Texno',
+      image:
+          'https://storage.kun.uz/source/8/5X73logbix8KzHlW2yu-16dIDHIRgCGd.jpg',
+      title: 'Honor buklanuvchi smartfonini taqdim etdi',
+      readingTime: 7,
+      isSaved: false,
+    ),
   ];
 }
